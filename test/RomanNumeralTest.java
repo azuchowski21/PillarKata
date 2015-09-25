11a9cbeab054f6ff6751e6cbba01d47bf8acca4d
azuchowski21 <andrewzuchowski@gmail.com>
2015-09-25 11:22:43 -0400

    refactored test to have a setup function to create reference to romanNumeral class

--- test/RomanNumeralTest.java
+++ test/RomanNumeralTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.Assert.*;
 /**/
@@ -6,40 +7,39 @@ import static org.junit.Assert.assertEquals;
 
 public class RomanNumeralTest
 {
+    RomanNumeral romanNumeral;
+    @Before
+    public void setUp()
+    {
+        romanNumeral = new RomanNumeral();
+    }
     @Test
     public void whenRomanNumeralIsPassedANumberItReturnsThatRomanNumeral()
     {
-        RomanNumeral romanNumeral = new RomanNumeral();
         assertEquals("I", romanNumeral.convertNumberToRomanNumeral(1));
 
     }
     @Test
     public void whenRomanNumeralIsPassedATwoItReturnsRomanNumeralForTwo()
     {
-        RomanNumeral romanNumeral = new RomanNumeral();
         assertEquals("II", romanNumeral.convertNumberToRomanNumeral(2));
 
     }
     @Test
     public void whenRomanNumeralIsPassedAThreeItReturnsRomanNumeralForThree()
     {
-        RomanNumeral romanNumeral = new RomanNumeral();
         assertEquals("III", romanNumeral.convertNumberToRomanNumeral(3));
 
     }
     @Test
     public void whenRomanNumeralIsPassedAFourItReturnsRomanNumeralForFour()
     {
-        RomanNumeral romanNumeral = new RomanNumeral();
         assertEquals("IV", romanNumeral.convertNumberToRomanNumeral(4));
-
     }
     @Test
     public void whenRomanNumeralIsPassedAFiveItReturnsRomanNumeralForFive()
     {
-        RomanNumeral romanNumeral = new RomanNumeral();
         assertEquals("V", romanNumeral.convertNumberToRomanNumeral(5));
-
     }
 
 }
